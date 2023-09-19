226f6b66ae17d00e7f52a370009666cbbaf16e07
Ngai Tik Chung <NgaitikK.Chung94@gmail.com>
2023-09-19 19:32:04 -0400

    js/index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,46 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+    fetchBooks()
+});
+
+function fetchBooks() {
+    fetch("url")
+    .then(resp=> resp.json())
+    .then(booksArr => booksArr.forEach(renderBookTitle))
+}
+
+function renderBookTitle(book) {
+    const bookTitleList = document.getElementById("List")
+    const bookTitle = document.createElement("li")
+    bookTitle.innerText = book.title
+    bookTitleList.append(bookTitle)
+    bookTitle.addEventListener("click", (book) => renderBookDetails(book))
+}
+
+
+function renderBookDetails(book) {
+    const {title, subtitle, description, author, img_url, users} = book
+    const bookDetailsDiv = document.querySelector('#show-panel')
+    
+    const bookTitle = document.createElement('h1')
+    const bookSub = document.createElement('h3')
+    const bookDes = document.createElement('p')
+    const bookAuthor = document.createElement('h3')
+    const bookImg = document.createElement('img')
+    
+    bookTitle.textContent = title
+    bookSub.textContent = subtitle
+    bookDes.textContent = description
+    bookAuthor.textContent = author
+    bookImg.src = img_url
+
+    const usersContainer = document.createElement('ul')
+
+    users.forEach((user) => {
+        console.log(user)
+        const userLi = document.createElement('li')
+        userLi.textContent = user.username
+        usersContainer.append(userLi)
+    })
+
+    bookDeetailsDiv.append(bookImg, bookTitle, bookSub, bookAuthor, bookDes, userContainer)
+}
